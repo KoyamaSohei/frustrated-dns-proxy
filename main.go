f0ac71600c9abcbb80ef4b18f9469c4bf854826d
KoyamaSohei <koyamaso0309@gmail.com>
2019-10-15 17:53:08 +0900

    refactor

--- main.go
+++ main.go
@@ -21,22 +21,28 @@ func main() {
 		m.SetReply(r)
 		cl := dns.Client{}
 		a := dns.Msg{}
-
-		a.SetQuestion(r.Question[0].Name, r.Question[0].Qtype)
-		res, _, err := cl.Exchange(&a, "8.8.8.8:53")
-		if err != nil || len(res.Answer) == 0 {
-			log.Println(err)
-			return
-		}
-		ans, ok := res.Answer[0].(*dns.A)
-		for !ok {
-			a.SetQuestion(res.Answer[0].(*dns.CNAME).Target, dns.TypeCNAME)
-			res, _, err = cl.Exchange(&a, "8.8.8.8:53")
+		var (
+			ok  bool
+			ans *dns.A
+		)
+		n := r.Question[0].Name
+		t := dns.TypeA
+		for ans == nil {
+			a.SetQuestion(n, t)
+			res, _, err := cl.Exchange(&a, "8.8.8.8:53")
 			if err != nil || len(res.Answer) == 0 {
-				log.Println(err)
 				return
 			}
+
 			ans, ok = res.Answer[0].(*dns.A)
+			if !ok {
+				cn, cok := res.Answer[0].(*dns.CNAME)
+				if !cok {
+					return
+				}
+				n = cn.Target
+				t = dns.TypeCNAME
+			}
 		}
 		rr := &dns.A{
 			Hdr: dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
